f13e3e6a07df476c33af82e64a4e0cb1e6c3c336
Ian <ian.brindley@ontrackms.com>
2025-01-08 19:37:44 +0000

    v1.0.2

--- publish/pack.sh
+++ publish/pack.sh
@@ -28,7 +28,7 @@ pack() {
     # compile package and copy files required by npm
     echo 'Building /src...'
     cd "$TO_SOURCE_DIR"
-    node_modules/.bin/tsc
+    npm run build # node_modules/.bin/tsc --skipLibCheck
     cd ..
 
     echo 'Creating package...'
